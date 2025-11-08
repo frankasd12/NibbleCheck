89d3daea11fc6bddf74f679d0a34c8b0b899a9a2
frankasd12 <frankiearisso721@gmail.com>
2025-11-08 13:52:59 -0500

    fix(sql): remove stray 'push' and use proper DO push ... push block

--- db/01_types.sql
+++ db/01_types.sql
@@ -1,6 +1,11 @@
-﻿DO push
+﻿-- db/01_types.sql
+
+-- Create enum food_status if it doesn't exist
+DO $$
 BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname='food_status') THEN
+  IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'food_status') THEN
     CREATE TYPE food_status AS ENUM ('SAFE','CAUTION','UNSAFE');
   END IF;
-END push;
+END
+$$ LANGUAGE plpgsql;
+
